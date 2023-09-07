0edba5e552c55cceb185ad48843c4f29eb2741ea
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-09-07 14:46:04 +0530

    first commit

---
+++ Practice_Project4/Practice_Project4/Data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Practice_Project4.Models;
+
+namespace Practice_Project4.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext (DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Practice_Project4.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Practice_Project4/Practice_Project4/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Practice_Project4.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Subject { get; set; }
+        public double Mark { get; set; }
+
+    }
+}
